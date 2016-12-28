63187960af56cae9c09e6163c60e2d29a0e95ed6
HoECoder <jphansen@gmail.com>
2016-12-28 17:22:46 -0600

    start adding a python module to hold test programs.

--- application/test_program_manager.py
+++ application/test_program_manager.py
@@ -2,4 +2,8 @@ import unittest
 
 from program_manager import program_manager
 
+new_path = r"D:\controller\config\test_programs"
 
+class TestDictionaryInterface(unittest.TestCase):
+    def setUp(self):
+        program_manager.change_program_path(new_path)
---
+++ application/test_program_manager_sample_programs.py
@@ -0,0 +1,86 @@
+simple_program_1 = """{
+    "station_duration": [
+        {
+            "duration": 5, 
+            "stid": 1
+        }, 
+        {
+            "duration": 5, 
+            "stid": 2
+        }, 
+        {
+            "duration": 5, 
+            "stid": 3
+        }, 
+        {
+            "duration": 5, 
+            "stid": 4
+        }, 
+        {
+            "duration": 5, 
+            "stid": 5
+        }, 
+        {
+            "duration": 5, 
+            "stid": 6
+        }, 
+        {
+            "duration": 5, 
+            "stid": 7
+        }, 
+        {
+            "duration": 5, 
+            "stid": 8
+        }
+    ], 
+    "interval": {
+        "type": "even"
+    }, 
+    "pid": 1, 
+    "time_of_day": "12:05:00",
+    "name": "Program 1"
+}"""
+
+simple_program_2 = """{
+    "station_duration": [
+        {
+            "duration": 5, 
+            "stid": 1
+        }, 
+        {
+            "duration": 5, 
+            "stid": 2
+        }, 
+        {
+            "duration": 5, 
+            "stid": 3
+        }, 
+        {
+            "duration": 5, 
+            "stid": 4
+        }, 
+        {
+            "duration": 5, 
+            "stid": 5
+        }, 
+        {
+            "duration": 5, 
+            "stid": 6
+        }, 
+        {
+            "duration": 5, 
+            "stid": 7
+        }, 
+        {
+            "duration": 5, 
+            "stid": 8
+        }
+    ], 
+    "interval": {
+        "type": "odd"
+    }, 
+    "pid": 2, 
+    "time_of_day": "12:05:00",
+    "name": "Program 2"
+}"""
+
