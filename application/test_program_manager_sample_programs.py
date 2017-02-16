fff2a18872e3e18caacdf6d74eb9ff2299842a62
HoECoder <JPHansen@gmail.com>
2017-02-16 15:41:14 -0600

    Fix up the JSON strings to make round-trip comparisons easier. Should really move to a dict compare, but that can come later.

--- application/test_program_manager_sample_programs.py
+++ application/test_program_manager_sample_programs.py
@@ -1,87 +1,89 @@
 simple_program_1 = """{
-    "station_duration": [
-        {
-            "duration": 5, 
-            "stid": 1
-        }, 
-        {
-            "duration": 5, 
-            "stid": 2
-        }, 
-        {
-            "duration": 5, 
-            "stid": 3
-        }, 
-        {
-            "duration": 5, 
-            "stid": 4
-        }, 
-        {
-            "duration": 5, 
-            "stid": 5
-        }, 
-        {
-            "duration": 5, 
-            "stid": 6
-        }, 
-        {
-            "duration": 5, 
-            "stid": 7
-        }, 
-        {
-            "duration": 5, 
-            "stid": 8
-        }
-    ], 
+    "pid": 1, 
+    "name": "Program 1", 
+    "enabled": true, 
+    "time_of_day": "12:05:00", 
     "interval": {
         "type": "even"
     }, 
-    "pid": 1, 
-    "time_of_day": "12:05:00",
-    "name": "Program 1"
+    "station_duration": [
+        {
+            "stid": 1, 
+            "duration": 5
+        }, 
+        {
+            "stid": 2, 
+            "duration": 5
+        }, 
+        {
+            "stid": 3, 
+            "duration": 5
+        }, 
+        {
+            "stid": 4, 
+            "duration": 5
+        }, 
+        {
+            "stid": 5, 
+            "duration": 5
+        }, 
+        {
+            "stid": 6, 
+            "duration": 5
+        }, 
+        {
+            "stid": 7, 
+            "duration": 5
+        }, 
+        {
+            "stid": 8, 
+            "duration": 5
+        }
+    ]
 }"""
 
 simple_program_2 = """{
-    "station_duration": [
-        {
-            "duration": 5, 
-            "stid": 1
-        }, 
-        {
-            "duration": 5, 
-            "stid": 2
-        }, 
-        {
-            "duration": 5, 
-            "stid": 3
-        }, 
-        {
-            "duration": 5, 
-            "stid": 4
-        }, 
-        {
-            "duration": 5, 
-            "stid": 5
-        }, 
-        {
-            "duration": 5, 
-            "stid": 6
-        }, 
-        {
-            "duration": 5, 
-            "stid": 7
-        }, 
-        {
-            "duration": 5, 
-            "stid": 8
-        }
-    ], 
+    "pid": 2, 
+    "name": "Program 2", 
+    "enabled": true, 
+    "time_of_day": "12:05:00", 
     "interval": {
         "type": "odd"
     }, 
-    "pid": 2, 
-    "time_of_day": "12:05:00",
-    "name": "Program 2"
+    "station_duration": [
+        {
+            "stid": 1, 
+            "duration": 5
+        }, 
+        {
+            "stid": 2, 
+            "duration": 5
+        }, 
+        {
+            "stid": 3, 
+            "duration": 5
+        }, 
+        {
+            "stid": 4, 
+            "duration": 5
+        }, 
+        {
+            "stid": 5, 
+            "duration": 5
+        }, 
+        {
+            "stid": 6, 
+            "duration": 5
+        }, 
+        {
+            "stid": 7, 
+            "duration": 5
+        }, 
+        {
+            "stid": 8, 
+            "duration": 5
+        }
+    ]
 }"""
 
 simple_programs = [simple_program_1, simple_program_2]
