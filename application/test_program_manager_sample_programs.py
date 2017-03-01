3c33c43184ba72adf518670271e1710865091f68
HoECoder <JPHansen@gmail.com>
2017-03-01 10:33:26 -0600

    Interim commit.

--- application/test_program_manager_sample_programs.py
+++ application/test_program_manager_sample_programs.py
@@ -87,3 +87,94 @@ simple_program_2 = """{
 }"""
 
 simple_programs = [simple_program_1, simple_program_2]
+
+short_day_dst_tester_program = """{
+    "pid": 1, 
+    "name": "Short Day Program", 
+    "enabled": true, 
+    "time_of_day": "01:59:50", 
+    "interval": {
+        "type": "odd"
+    }, 
+    "station_duration": [
+        {
+            "stid": 1, 
+            "duration": 5
+        }, 
+        {
+            "stid": 2, 
+            "duration": 5
+        }, 
+        {
+            "stid": 3, 
+            "duration": 5
+        }, 
+        {
+            "stid": 4, 
+            "duration": 5
+        }, 
+        {
+            "stid": 5, 
+            "duration": 5
+        }, 
+        {
+            "stid": 6, 
+            "duration": 5
+        }, 
+        {
+            "stid": 7, 
+            "duration": 5
+        }, 
+        {
+            "stid": 8, 
+            "duration": 5
+        }
+    ]
+}
+"""
+long_day_dst_tester_program = """{
+    "pid": 2, 
+    "name": "Long Day Program", 
+    "enabled": true, 
+    "time_of_day": "01:59:50", 
+    "interval": {
+        "type": "odd"
+    }, 
+    "station_duration": [
+        {
+            "stid": 1, 
+            "duration": 5
+        }, 
+        {
+            "stid": 2, 
+            "duration": 5
+        }, 
+        {
+            "stid": 3, 
+            "duration": 5
+        }, 
+        {
+            "stid": 4, 
+            "duration": 5
+        }, 
+        {
+            "stid": 5, 
+            "duration": 5
+        }, 
+        {
+            "stid": 6, 
+            "duration": 5
+        }, 
+        {
+            "stid": 7, 
+            "duration": 5
+        }, 
+        {
+            "stid": 8, 
+            "duration": 5
+        }
+    ]
+}
+"""
+
+dst_programs = [short_day_dst_tester_program, long_day_dst_tester_program]
--- application/test_utc_transitions.py
+++ application/test_utc_transitions.py
@@ -6,6 +6,12 @@ from calendar import timegm
 import pytz
 
 from clock import *
+from settings import settings
+from program_manager import program_manager
+from controller import Controller
+from core_config import program_name_glob, program_name_template
+from test_program_manager import new_path, make_program_full_path
+from test_program_manager_sample_programs import *
 
 central = None
 
@@ -27,6 +33,29 @@ long_tick_count = len(long_sample_times)
 
 fmt = '%Y-%m-%d %H:%M:%S %Z'
 
+def setUpProgramManager():
+    program_manager.change_program_path(new_path)
+    masterTearDown()
+    i = 0
+    while i < len(dst_programs):
+        program_path = make_program_full_path(i + 1)
+        if os.path.exists(program_path):
+            os.remove(program_path)
+        f = open(program_path, "wb")
+        f.write(dst_programs[i])
+        f.flush()
+        f.close()
+        del f
+        i += 1
+    program_manager.load_programs()
+    
+def tearDownProgramManager():
+    prog_glob = program_manager.program_glob
+    program_paths = glob.glob(prog_glob)
+    for program_path in program_paths:
+        if os.path.exists(program_path):
+            os.remove(program_path)
+
 def setUpModule():
     sim_clock.tick_unit = 1
     # Make Central Time
@@ -47,15 +76,17 @@ def setUpModule():
     #print short_days[-1]
     #print long_days[-1]
 
-class TestClockShortDaySimple(unittest.TestCase):
-    def __test_single_transition(self, transition, check_list):
+class TestClockShortLongDaySimple(unittest.TestCase):
+    def __test_single_transition(self, transition, check_list, debug_print = False):
         sim_clock.set_arbitrary_time_float(transition)
-        #print '====='
+        if debug_print:
+            print '====='
         seconds_from_midnight = list()
         for x in xrange(len(check_list)):
             now = make_now()
-            #s = ("%s, %s" % (time.strftime(fmt,time.localtime(now['epoch'])), pretty_now(now)))
-            #print s
+            if debug_print:
+                s = ("%s, %s" % (time.strftime(fmt,time.localtime(now['epoch'])), pretty_now(now)))
+                print s
             seconds_from_midnight.append(now['seconds_from_midnight'])
             sim_clock.tick()
         msg = "Bad Transition: %0.1f" % transition
@@ -63,11 +94,11 @@ class TestClockShortDaySimple(unittest.TestCase):
     
     def test_one_short_transition(self):
         last = short_days[-1]
-        self.__test_single_transition(last, short_sample_times)
+        self.__test_single_transition(last, short_sample_times, True)
 
     def test_one_long_transition(self):
         last = long_days[-1]
-        self.__test_single_transition(last, long_sample_times)
+        self.__test_single_transition(last, long_sample_times, True)
 
     def test_short_transitions(self):
         for day in short_days:
@@ -78,12 +109,24 @@ class TestClockShortDaySimple(unittest.TestCase):
             self.__test_single_transition(day, long_sample_times)
             
 
+class TestProgramStatesOnTransition(unittest.TestCase):
+    def setUp(self):
+        settings.load()
+        controller = Controller()
+        program_manager.bind_stations(controller.stations)
+        self.controller = controller
+    def test_single_transition(self, transition, check_list):
+        sim_clock.set_arbitrary_time_float(transition)
+        #print '====='
+        
+        
+
 def load_tests(loader, tests, pattern):
     
     dict_suite = unittest.TestSuite()
-    dict_suite.addTest(TestClockShortDaySimple('test_one_short_transition'))
-    dict_suite.addTest(TestClockShortDaySimple('test_one_long_transition'))
-    dict_suite.addTest(TestClockShortDaySimple('test_short_transitions'))
-    dict_suite.addTest(TestClockShortDaySimple('test_long_transitions'))
+    dict_suite.addTest(TestClockShortLongDaySimple('test_one_short_transition'))
+    dict_suite.addTest(TestClockShortLongDaySimple('test_one_long_transition'))
+    dict_suite.addTest(TestClockShortLongDaySimple('test_short_transitions'))
+    dict_suite.addTest(TestClockShortLongDaySimple('test_long_transitions'))
     
     return unittest.TestSuite([dict_suite])
